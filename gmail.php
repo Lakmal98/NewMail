60e78524d055f549282f6b33ea5cadc9cd607993
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-05-18 20:21:22 +0530

    message Filtering implemented

--- gmail.php
+++ gmail.php
@@ -1,6 +1,8 @@
 <?php
 
 class Gmail {
+    
+    
     public function __construct($client) {
         $this->client = $client;
     }
@@ -51,7 +53,7 @@ class Gmail {
   $i=0;  
         foreach ($messages as $message) {
             $this->getMessage($service, $userId, $message->getId());
-        if($i==15){
+        if($i==1){
         break;
         } else {
             $i++;
@@ -65,8 +67,18 @@ class Gmail {
     function getMessage($service, $userId, $messageId) {
         try {
           $message = $service->users_messages->get($userId, $messageId);
-          echo "<pre>";
-          print_r($message);
+
+          $filterLabels = array("UNREAD", "INBOX");//Filter this labels
+        //   $fiterIncludes = array("UGVLE", "IS21", "assignment")
+          $snippet = $message->snippet;
+          $labels = $message->labelIds;
+        //   if(in_array($filterLabels[0], $labels) && in_array($filterLabels[1], $labels)) {
+            if(in_array($filterLabels[1], $labels)) {
+                if (preg_match('[UGVLE|IS21]', $snippet )) {
+                    $messageId = $message->id;
+                }
+          }
+        //   echo($message->snippet);
           return $message;
         } catch (Exception $e) {
           print 'An error occurred: ' . $e->getMessage();
